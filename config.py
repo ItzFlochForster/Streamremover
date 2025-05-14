be5f653f2125c83952d93c148100fcbe38e3c2ef
ItzFlochForster <Meowtaro6659@gmail.com>
2025-05-15 00:10:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "10811400")
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8163251890:AAGpC1g4Hi_PCRd352LwmuK-6aS9DZzcwIA")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8179604363:AAHUvF_Rs95t23XT057nfhh8ROVBXYHRhhQ")
 CAPTION = os.environ.get("CAPTION", "")
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://HARSHA24:HARSHA24@cluster0.sxaj8up.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = os.environ.get("DATABASE_URI", "naomi")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
-ADMIN = int(os.environ.get("ADMIN", '6469754522'))
+ADMIN = int(os.environ.get("ADMIN", 6440021089'))
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 SUNRISES_PIC= "https://envs.sh/lHg.jpg"  # Replace with your Telegraph link
-LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
+LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002134572304)
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8081")) #8080
 
